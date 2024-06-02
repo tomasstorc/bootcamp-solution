3ffdbd2c07689c1d158c481cb12fb8a030833a4e
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 15:34:17 +0200

    fix: identity index

--- rbac.tf
+++ rbac.tf
@@ -2,7 +2,7 @@
 resource "azurerm_role_assignment" "kv_rbac" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = azurerm_windows_virtual_machine.win_vm.identity.principal_id
+  principal_id         = azurerm_windows_virtual_machine.win_vm.identity[0].principal_id
 }
 
 #give yourself ad access to vm
