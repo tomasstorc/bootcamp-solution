4314dd855ed9ba3a0e7f325b88be9d47948b6b92
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 20:10:59 +0200

    edit nsg

--- nsg.tf
+++ nsg.tf
@@ -14,54 +14,10 @@ resource "azurerm_network_security_group" "nsg_rdp" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "DenyAll"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+
 
 }
 
-resource "azurerm_network_security_group" "nsg_psql" {
-  name                = "ts-test-bootcamp-nsg2"
-  location            = azurerm_resource_group.bootcamp_rg.location
-  resource_group_name = azurerm_resource_group.bootcamp_rg.name
-
-  security_rule {
-    name                       = "allowPostgre"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5432"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "DenyAll"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_subnet_network_security_group_association" "psql" {
-  subnet_id                 = azurerm_subnet.psql_subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg_psql.id
-}
 
 resource "azurerm_subnet_network_security_group_association" "rdp" {
   subnet_id                 = azurerm_subnet.pe_subnet.id
