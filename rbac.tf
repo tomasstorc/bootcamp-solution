75e6817836dc3fe1098a271f2e6147fc4c65b6f5
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 15:11:56 +0200

    fix: rename rbac

--- rbac.tf
+++ rbac.tf
@@ -1,12 +1,12 @@
 # access from vm to key vault
-resource "azurerm_role_assignment" "example" {
+resource "azurerm_role_assignment" "kv_rbac" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = azurerm_windows_virtual_machine.win_vm.identity.principal_id
 }
 
 #give yourself ad access to vm
-resource "azurerm_role_assignment" "example" {
+resource "azurerm_role_assignment" "vm_rbac" {
   scope                = azurerm_windows_virtual_machine.win_vm.id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = data.azuread_user.myself.object_id
