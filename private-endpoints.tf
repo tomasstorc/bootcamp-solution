4d5e1ba93a041a773c4896c14e954b5dda760f97
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 19:01:00 +0200

    feat: remove sql pe

--- private-endpoints.tf
+++ private-endpoints.tf
@@ -1,21 +1,4 @@
-resource "azurerm_private_endpoint" "sql_pe" {
-  name                = "private-endpoint-sql"
-  location            = azurerm_resource_group.bootcamp_rg.location
-  resource_group_name = azurerm_resource_group.bootcamp_rg.name
-  subnet_id           = azurerm_subnet.sql_subnet.id
 
-  private_service_connection {
-    name                           = "ts-test-bootcamp-sql-psc"
-    private_connection_resource_id = azurerm_mssql_server.server.id
-    subresource_names              = ["sqlServer"]
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.sql_dns.id]
-  }
-}
 
 resource "azurerm_private_endpoint" "pe_kv" {
   name                = "ts-test-bootcamp-kv-pen"
