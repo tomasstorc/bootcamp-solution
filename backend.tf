518a68159d1eb0adaec13b6aa6e634ec7b8f60af
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 16:10:47 +0200

    feat: change client id for backend config

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
-
+    client_id            = "af6b77c1-9b4e-4463-91a6-92599feda31d"
   }
 }
