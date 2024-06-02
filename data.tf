afd5f55ee50cbb7f0f8904d763df10c3a240d332
Tomas Storc <tomas.storc@gmail.com>
2024-06-02 15:53:39 +0200

    remove azureaad

--- data.tf
+++ data.tf
@@ -1,12 +1,6 @@
 # info about current user
 data "azurerm_client_config" "current" {}
-data "azuread_user" "current_user" {
-  object_id = data.azurerm_client_config.current.object_id
-}
 
-data "azuread_user" "myself" {
-  object_id = var.user_id
-}
 
 # generate random pw
 resource "random_password" "password" {
