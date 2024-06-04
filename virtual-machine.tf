d59c51503161e712c1edc617b44f2d7ccee8f835
Tomas Storc <tomas.storc@gmail.com>
2024-06-04 17:29:42 +0200

    feat: remove aad extension

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -25,11 +25,3 @@ resource "azurerm_windows_virtual_machine" "win_vm" {
   }
 }
 
-resource "azurerm_virtual_machine_extension" "aad_login" {
-  name                       = "vmext-AADLoginForWindows"
-  virtual_machine_id         = azurerm_windows_virtual_machine.win_vm.id
-  publisher                  = "Microsoft.Azure.ActiveDirectory"
-  type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
-  auto_upgrade_minor_version = true
-}
