2f061cdf3f393592d061260157e495646e3cff8a
Tomas Storc <tomas.storc@gmail.com>
2024-06-09 21:07:46 +0200

    fix: comma

--- vm-extensions.tf
+++ vm-extensions.tf
@@ -32,7 +32,7 @@ resource "azurerm_virtual_machine_extension" "CSE" {
 
   settings = <<SETTINGS
  {
-  "fileUris": [var.script_uri]
+  "fileUris": [var.script_uri],
   "commandToExecute": "powershell.exe installPython.ps1"
  }
 SETTINGS
